c202a218df97dc1527eb03164a044c04e134f525
Artoria Stevens <artoriastevens@gmail.com>
2025-05-24 19:42:26 +0100

    Update to correctly respond with error codes

--- ps_challenge/app.py
+++ ps_challenge/app.py
@@ -1,23 +1,26 @@
 import logging
 
-from flask import Flask
+from flask import Flask, make_response
 
 from ps_challenge.controller import get_user_achievement_level
 from ps_challenge.exceptions import UserDataNotFoundError
+
 
 logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def get_all_users():
-    return {}
-
-
 @app.get("/users")
 def get_all_users_levels():
-    return {}
+    try:
+        app.logger.info("Received get for all users")
+        return {}
+    except UserDataNotFoundError as err:
+        resp = make_response(
+            {"message": err.msg, "status_code": err.status_code}, err.status_code
+        )
+        return resp
 
 
 @app.get("/users/<int:user_id>")
@@ -26,4 +29,7 @@ def get_user_level(user_id):
         app.logger.info("Received get for user with ID %s", user_id)
         return get_user_achievement_level(user_id)
     except UserDataNotFoundError as err:
-        return {"message": err.msg, "status_code": err.status_code}
+        resp = make_response(
+            {"message": err.msg, "status_code": err.status_code}, err.status_code
+        )
+        return resp
